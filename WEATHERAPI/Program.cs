9a32a4e23cf0c6ab392913fe1b301820d004ae36
Alejandro Alatorre <alejandro.c.alatorre@gmail.com>
2023-04-10 11:55:09 -0700

    Add project files.

---
+++ WEATHERAPI/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Net.WebRequestMethods;
+
+namespace WEATHERAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var weatherURL = "https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}";
+            var weatherResponse = client.GetStringAsync(weatherURL).Result;
+            Console.WriteLine(weatherResponse);
+
+
+
+        }
+    }
+}
