fa653b958dca18124c5a00acea90871cd53814f6
Alejandro Alatorre <alejandro.c.alatorre@gmail.com>
2023-04-10 21:06:03 -0700

    Weatherman to the rescue

--- WEATHERAPI/Program.cs
+++ WEATHERAPI/Program.cs
@@ -1,4 +1,7 @@
-﻿using static System.Net.WebRequestMethods;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System.Threading.Channels;
+
 
 namespace WEATHERAPI
 {
@@ -6,11 +9,40 @@ namespace WEATHERAPI
     {
         static void Main(string[] args)
         {
-            var client = new HttpClient();
-            var weatherURL = "https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}";
-            var weatherResponse = client.GetStringAsync(weatherURL).Result;
-            Console.WriteLine(weatherResponse);
+            string key = File.ReadAllText("appsettings.json");
+            string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
 
+            var cont = true;
+            do
+            {
+                Console.WriteLine("Please enter zipcode to get current weather conditions!");
+                var zipCode = Console.ReadLine();
+                Console.WriteLine();
+                string apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIkey}&units=imperial";
+                Console.WriteLine("()()()()()()()()()()()()()()()()()()()()()()()()()()()");
+                Console.WriteLine($"Weather for {Weather.City(apiCall)}!");
+                Console.WriteLine("======================================================");
+                Console.WriteLine($"Current condition: {Weather.Descript(apiCall)}");
+                Console.WriteLine($"Current temperature: {Weather.Temp(apiCall)}°F");
+                Console.WriteLine($"Feels like: {Weather.FeelsLike(apiCall)}°F");
+                Console.WriteLine($"Today's low: {Weather.Min(apiCall)}°F");
+                Console.WriteLine($"Today's high: {Weather.Max(apiCall)}°F");
+                Console.WriteLine($"Humidity: {Weather.Humid(apiCall)}%");
+                Console.WriteLine("======================================================");
+                Console.WriteLine();
+
+                Console.WriteLine("Would you like to check weather for a diferent zip code?");
+                Console.WriteLine("Y or N?");
+                Console.WriteLine();
+
+                var userAns = Console.ReadLine().ToLower();
+
+                cont = (userAns == "n") ? false : true;
+            } while (cont);
+
+            Console.WriteLine();
+            Console.WriteLine("Have a wonderful day!");
+            Console.WriteLine("()()()()()()()()()()()()()()()()()()()()()()()()()()()");
 
 
         }
--- WEATHERAPI/Weather.cs
+++ WEATHERAPI/Weather.cs
@@ -1,14 +1,71 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace WEATHERAPI
 {
     public static class Weather
     {
+        public static double Temp(string apiCall)
+        {
+            var client = new HttpClient();
 
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+            
+
+        }
+        public static double Min(string apiCall)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(apiCall).Result;
+            var min = double.Parse(JObject.Parse(response)["main"]["temp_min"].ToString());
+            return min;
+
+
+        }
+        public static double Max(string apiCall)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(apiCall).Result;
+            var max = double.Parse(JObject.Parse(response)["main"]["temp_max"].ToString());
+            return max;
+        }
+        public static string Descript(string apiCall)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(apiCall).Result;
+            var descript = JObject.Parse(response)["weather"][0]["main"].ToString();
+            return descript;
+        }
+        public static string City(string apiCall)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(apiCall).Result;
+            var city = JObject.Parse(response)["name"].ToString();
+            return city;
+        }
+        public static double Humid(string apiCall)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(apiCall).Result;
+            var humid = double.Parse(JObject.Parse(response)["main"]["humidity"].ToString());
+            return humid;
+        }
+        public static double FeelsLike(string apiCall)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(apiCall).Result;
+            var feel = double.Parse(JObject.Parse(response)["main"]["feels_like"].ToString());
+            return feel;
+        }
 
     }
 }
